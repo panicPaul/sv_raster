320c33a4cf09a1b00906dc6743f16bfef6deb6d3
Paul Schlack <schlack@8sgqfd4.fe.hhi.de>
2026-04-15 11:13:11 +0200

    Fix new_cuda CUDA header include paths

--- backends/new_cuda/setup.py
+++ backends/new_cuda/setup.py
@@ -1,9 +1,20 @@
 import os
+from pathlib import Path
+
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+
 cuda_tag = os.environ.get("SVRASTER_CUDA_TAG", "")
 ext_name = f"new_svraster_cuda._C_{cuda_tag}" if cuda_tag else "new_svraster_cuda._C"
+cuda_home = Path(os.environ.get("CUDA_HOME", "/usr/local/cuda"))
+cuda_target_include = cuda_home / "targets" / "x86_64-linux" / "include"
+cuda_cccl_include = cuda_target_include / "cccl"
+
+include_dirs = []
+for path in (cuda_target_include, cuda_cccl_include):
+    if path.exists():
+        include_dirs.append(str(path))
 
 setup(
     name="new_svraster_cuda",
@@ -21,8 +32,9 @@ setup(
                 "src/tv_compute.cu",
                 "src/utils.cu",
                 "src/adam_step.cu",
-                "binding.cpp"
+                "binding.cpp",
             ],
+            include_dirs=include_dirs,
         )
     ],
     cmdclass={"build_ext": BuildExtension},
